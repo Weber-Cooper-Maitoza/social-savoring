31cd1b213cd9be3d55901144bbe8047baacd66b3
Cooper-Maitoza <coopermaitoza@mail.weber.edu>
2024-12-04 15:53:55 -0700

    finally got images to upload, delete, and update

--- socialSavoring/recipies/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.forms import inlineformset_factory
-from .models import Recipe, Ingredient
-
-# class RecipeForm(forms.ModelForm):
-#   class Meta:
-#     model = Recipe
-#     # TODO: add fields
-#     fields = []
--- socialSavoring/recipies/views.py
+++ socialSavoring/recipies/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .models import Recipe, Ingredient
 from profiles.models import Profile
 from datetime import datetime
+import os
 
 # Create your views here.
 
@@ -22,18 +23,16 @@ def recipe_detail_view(request, id):
 @login_required
 def recipe_create_view(request, id):
   if request.method == 'POST':
-    # creator = models.ForeignKey(Profile, on_delete=models.CASCADE)
     creator = Profile.objects.get(pk=id)
     created_timestamp = datetime.now()
     recipe_name = request.POST.get('recipe_name', '')
-    # recipe_image = models.CharField(max_length=500, default="")
-    # recipe_image = request.POST.get('recipe_image', '')
+    recipe_image = request.FILES.get('recipe_image', 'empty.jpg')
     oven_temp = request.POST.get('oven_temp', '')
     serving_size = request.POST.get('serving_size', '')
     recipe_category = request.POST.get('recipe_category', '')
     recipe_tags = request.POST.get('recipe_tags', '')
     recipe_instructions = request.POST.get('recipe_instructions', '')
-    recipe = Recipe(creator=creator, created_timestamp=created_timestamp, recipe_name=recipe_name, oven_temp=oven_temp, serving_size=serving_size, recipe_category=recipe_category, recipe_tags=recipe_tags, recipe_instructions=recipe_instructions)
+    recipe = Recipe(creator=creator, created_timestamp=created_timestamp, recipe_name=recipe_name, recipe_image=recipe_image, oven_temp=oven_temp, serving_size=serving_size, recipe_category=recipe_category, recipe_tags=recipe_tags, recipe_instructions=recipe_instructions)
     recipe.save()
     return redirect("recipies:my_recipes", id=id)
   return render(request, 'recipes/create_recipe.html')
@@ -42,18 +41,25 @@ def recipe_create_view(request, id):
 @login_required
 def recipe_update_view(request, id):
   recipe = Recipe.objects.get(pk=id)
+  user_id = recipe.creator.id
   if request.method == 'POST':
+    image_path = recipe.recipe_image.path
+    if os.path.exists(image_path):
+      os.remove(image_path)
+    creator = Profile.objects.get(pk=user_id)
+    created_timestamp = recipe.created_timestamp
     updated_timestamp = datetime.now()
     recipe_name = request.POST.get('recipe_name', '')
-    # recipe_image = models.CharField(max_length=500, default="")
-    # recipe_image = request.POST.get('recipe_image', '')
+    recipe_image = request.FILES.get('recipe_image', 'empty.jpg')
     oven_temp = request.POST.get('oven_temp', '')
     serving_size = request.POST.get('serving_size', '')
     recipe_category = request.POST.get('recipe_category', '')
     recipe_tags = request.POST.get('recipe_tags', '')
     recipe_instructions = request.POST.get('recipe_instructions', '')
-    Recipe.objects.filter(pk=id).update(updated_timestamp=updated_timestamp, recipe_name=recipe_name, oven_temp=oven_temp, serving_size=serving_size, recipe_category=recipe_category, recipe_tags=recipe_tags, recipe_instructions=recipe_instructions)
-    return redirect("recipies:my_recipes", id=recipe.creator.id)
+    newRecipe = Recipe(creator=creator, created_timestamp=created_timestamp, updated_timestamp=updated_timestamp, recipe_name=recipe_name, recipe_image=recipe_image, oven_temp=oven_temp, serving_size=serving_size, recipe_category=recipe_category, recipe_tags=recipe_tags, recipe_instructions=recipe_instructions)
+    newRecipe.save()
+    recipe.delete()
+    return redirect("recipies:my_recipes", id=user_id)
   return render(request, 'recipes/recipe_edit.html', {'recipe': recipe})
 
 
@@ -63,5 +69,8 @@ def recipe_delete_view(request, id):
 
   if request.method == 'POST':
     recipe.delete()
+    image_path = recipe.recipe_image.path
+    if os.path.exists(image_path):
+        os.remove(image_path)
     return redirect("recipies:my_recipes", id=recipe.creator.id)
   return render(request, 'recipes/recipe_delete.html', {'recipe': recipe})
