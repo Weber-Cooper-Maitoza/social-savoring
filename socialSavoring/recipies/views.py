8a85df7bff535ee382416ee53669d36a85f9bf11
Cooper-Maitoza <coopermaitoza@mail.weber.edu>
2024-12-07 13:54:44 -0700

    working! need to fix edit ingredients in recipe.

--- socialSavoring/recipies/views.py
+++ socialSavoring/recipies/views.py
@@ -18,9 +18,11 @@ def recipe_list_view(request, id):
 
 @login_required
 def recipe_detail_view(request, id):
-  profile = Profile.objects.get(user=User.objects.get(id=id))
+  # profile = Profile.objects.get(user=User.objects.get(id=id))
   recipe = Recipe.objects.get(pk=id)
-  return render(request, 'recipes/recipe_detail.html', {'recipe': recipe, 'profile': profile})
+  profile = recipe.creator
+  ingredient_list = Ingredient.objects.filter(recipe=recipe.id)
+  return render(request, 'recipes/recipe_detail.html', {'recipe': recipe, 'ingredient_list': ingredient_list, 'profile': profile})
 
 
 @login_required
@@ -43,16 +45,17 @@ def recipe_create_view(request, id):
     for i in range(0, int(ingredient_count) + 1):
       ingredient_name = request.POST.get('ingredient_name_' + str(i), 'didnt work')
       ingredient_quantitiy = request.POST.get('ingredient_quantitiy_' + str(i), '0')
-      ingredient_measurement = request.POST.get('ingredient_measurement_' + str(i), '0')
-      new_ingredient = Ingredient(recipe=recipe, ingredient_name=ingredient_name, ingredient_quanitiy=float(ingredient_quantitiy), ingredient_measurement=int(ingredient_measurement))
+      ingredient_measurement = request.POST.get('ingredient_measurement_' + str(i), '')
+      new_ingredient = Ingredient(recipe=recipe, ingredient_name=ingredient_name, ingredient_quanitiy=float(ingredient_quantitiy), ingredient_measurement=ingredient_measurement)
       new_ingredient.save()
     return redirect("recipies:my_recipes", id=id)
   return render(request, 'recipes/create_recipe.html', {'profile': profile})
 
 @login_required
 def recipe_update_view(request, id):
-  profile = Profile.objects.get(user=User.objects.get(id=id))
   recipe = Recipe.objects.get(pk=id)
+  ingredient_list = Ingredient.objects.filter(recipe=recipe.id)
+  profile = recipe.creator
   user_id = recipe.creator.id
   if request.method == 'POST':
     image_path = recipe.recipe_image.path
@@ -72,7 +75,7 @@ def recipe_update_view(request, id):
     newRecipe.save()
     recipe.delete()
     return redirect("recipies:my_recipes", id=user_id)
-  return render(request, 'recipes/recipe_edit.html', {'recipe': recipe, 'profile': profile})
+  return render(request, 'recipes/recipe_edit.html', {'recipe': recipe, 'ingredient_list': ingredient_list, 'profile': profile})
 
 
 @login_required
